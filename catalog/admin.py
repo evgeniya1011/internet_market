cc61f3395df76d2f96157a8c916030e1f7773465
evgeniya1011 <zhdavydova@mail.ru>
2023-09-01 15:12:14 +0500

    add admin.py(hw20.1)

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category, Contacts
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'product_name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('product_name', 'descriptions',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'category_name',)
+
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('contact_name', 'ph_number', 'message',)
