b60c174a06c071427ba63df2ba7c1bc26611c03d
evgeniya1011 <zhdavydova@mail.ru>
2023-09-01 15:10:47 +0500

    fix views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 import json
+from catalog.models import Product, Contacts
+
 
 def home(request):
+    last_products = Product.objects.order_by('-date_create')[:5]
+    for product in last_products:
+        print(product.product_name)
     return render(request, 'catalog/home.html')
 
 def contacts(request):
+    contact_list = []
     if request.method == "POST":
         name = request.POST.get("name")
         ph_number = request.POST.get("phone")
         message = request.POST.get("message")
-        print(f'Новое сообщение от  {name}({ph_number}): {message}')
+        Contacts.objects.create(contact_name=name, ph_number=ph_number, message=message)
     return render(request, 'catalog/contacts.html')
