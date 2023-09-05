ed2162d8378e7ce0817c22ddeb5200a9ce494366
evgeniya1011 <zhdavydova@mail.ru>
2023-09-05 11:56:56 +0500

    add my_tag (hw20.2)

---
+++ catalog/templatetags/my_tags.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+@register.filter()
+def mediapath(val):
+    if val:
+        return f'/media/{val}'
+    return '#'
+
+@register.simple_tag
+def mediapath(val):
+    return f'/media/{val}'
+
