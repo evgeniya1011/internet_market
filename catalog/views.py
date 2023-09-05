b75d982beb8ba81540c47eabf76987cea28055e2
evgeniya1011 <zhdavydova@mail.ru>
2023-09-05 11:53:46 +0500

    fix home, add product (hw20.2)

--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,47 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
-import json
-from catalog.models import Product, Contacts
+from catalog.models import Product, Contacts, Category
 
 
 def home(request):
-    last_products = Product.objects.order_by('-date_create')[:5]
-    for product in last_products:
-        print(product.product_name)
-    return render(request, 'catalog/home.html')
+    if request.method == "POST":
+        product_name = request.POST.get("name")
+        descriptions = request.POST.get("descriptions")
+        picture = request.POST.get("picture")
+        category = request.POST.get("category")
+        category_inst = Category.objects.get(pk=category)
+        price = request.POST.get("price")
+        date_create = request.POST.get("date_create")
+        date_update = request.POST.get("date_update")
+        Product.objects.create(product_name=product_name, descriptions=descriptions, picture=picture, category=category_inst, price=price, date_create=date_create, date_update=date_update)
+
+    last_products = Product.objects.order_by('-date_create')
+    for item in last_products[:5]:
+        print(item.product_name)
+
+    products = last_products.all()
+    paginator = Paginator(products, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'title': 'Наши товары'
+    }
+    return render(request, 'catalog/home.html', context)
+
 
 def contacts(request):
-    contact_list = []
     if request.method == "POST":
         name = request.POST.get("name")
         ph_number = request.POST.get("phone")
         message = request.POST.get("message")
         Contacts.objects.create(contact_name=name, ph_number=ph_number, message=message)
     return render(request, 'catalog/contacts.html')
+
+
+def product(request, pk):
+    product_name = Product.objects.get(pk=pk)
+    context = {
+        'object': product_name
+    }
+    return render(request, 'catalog/product.html', context)
