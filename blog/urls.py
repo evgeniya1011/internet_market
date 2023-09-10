c009e3180f439b8cfe6b71dfbbcd7cf2487bab1e
evgeniya1011 <zhdavydova@mail.ru>
2023-09-11 01:55:20 +0500

    add blog/urls.py(hw21.1)

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from blog.apps import BlogConfig
+from blog.views import ArticleCreateView, ArticleListView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
+
+app_name = BlogConfig.name
+urlpatterns = [
+    path('create/', ArticleCreateView.as_view(), name='create'),
+    path('', ArticleListView.as_view(), name='list'),
+    path('view/<int:pk>/', ArticleDetailView.as_view(), name='view'),
+    path('edit/<int:pk>/', ArticleUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>/', ArticleDeleteView.as_view(), name='delete')
+]
