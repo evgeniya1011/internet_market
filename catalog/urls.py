61d481ad4a63e169a9d2c23d3abeadad78527790
evgeniya1011 <zhdavydova@mail.ru>
2023-09-11 01:52:40 +0500

    fix catalog/urls.py(hw21.1)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product
+from catalog.views import ProductDetailViews, ProductCreateView, ContactsCreateView
 
 app_name = CatalogConfig.name
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts),
-    path('<int:pk>/product/', product, name='product')
+    path('', ProductCreateView.as_view(), name='home'),
+    path('contacts/', ContactsCreateView.as_view(), name='contacts'),
+    path('<int:pk>/product/', ProductDetailViews.as_view(), name='view')
+    # path('create/', ProductCreateView.as_view(), name='create')
 ]
