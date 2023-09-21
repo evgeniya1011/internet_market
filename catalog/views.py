e8ed4a5cf1bc9e9a64a9baf4caf4a65e0611ddf4
evgeniya1011 <zhdavydova@mail.ru>
2023-09-21 13:58:15 +0500

    fix views.py(hw22.1)

--- catalog/views.py
+++ catalog/views.py
@@ -1,43 +1,20 @@
-from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView, CreateView, ListView
 
-from catalog.models import Product, Contacts, Category
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, Contacts, Category, Version
 
 
-# def home(request):
-#     if request.method == "POST":
-#         product_name = request.POST.get("name")
-#         descriptions = request.POST.get("descriptions")
-#         picture = request.POST.get("picture")
-#         category = request.POST.get("category")
-#         category_inst = Category.objects.get(pk=category)
-#         price = request.POST.get("price")
-#         date_create = request.POST.get("date_create")
-#         date_update = request.POST.get("date_update")
-#         Product.objects.create(product_name=product_name, descriptions=descriptions, picture=picture, category=category_inst, price=price, date_create=date_create, date_update=date_update)
-#
-#     last_products = Product.objects.order_by('-date_create')
-#     for item in last_products[:5]:
-#         print(item.product_name)
-#
-#     products = last_products.all()
-#     paginator = Paginator(products, 3)
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-#     context = {
-#         'page_obj': page_obj,
-#         'title': 'Наши товары'
-#     }
-#     return render(request, 'catalog/home.html', context)
-
-
-class ProductCreateView(CreateView, ListView):
+class ProductCreateView(CreateView):
     model = Product
-    template_name = "catalog/home.html"
-    fields = ["product_name", "descriptions", "picture", "category", "price"]
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:list')
+
+
+class ProductListView(ListView):
+    model = Product
     content_object_name = 'page_obj'
     paginate_by = 3
 
@@ -45,38 +22,59 @@ class ProductCreateView(CreateView, ListView):
         return Product.objects.order_by('-date_create')
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Наши товары'
-        return context
-
-    def post(self, request):
-        product_name = request.POST.get("name")
-        descriptions = request.POST.get("descriptions")
-        picture = request.POST.get("picture")
-        category = request.POST.get("category")
-        category_inst = Category.objects.get(pk=category)
-        price = request.POST.get("price")
-        date_create = request.POST.get("date_create")
-        date_update = request.POST.get("date_update")
-        Product.objects.create(
-            product_name=product_name,
-            descriptions=descriptions,
-            picture=picture,
-            category=category_inst,
-            price=price,
-            date_create=date_create,
-            date_update=date_update
-        )
-        return HttpResponseRedirect(self.request.path_info)
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = 'Наши товары'
+        return context_data
 
 
-# def contacts(request):
-#     if request.method == "POST":
-#         name = request.POST.get("name")
-#         ph_number = request.POST.get("phone")
-#         message = request.POST.get("message")
-#         Contacts.objects.create(contact_name=name, ph_number=ph_number, message=message)
-#     return render(request, 'catalog/contacts.html')
+class VersionCreateView(CreateView):
+    model = Version
+    form_class = VersionForm
+
+    def get_success_url(self, *args, **kwargs):
+        product_pk = self.object.product.pk
+        return reverse('catalog:view_version', kwargs={'pk': product_pk})
+
+
+class VersionListView(ListView):
+    model = Version
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(product=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+
+        product_item = Product.objects.get(pk=self.kwargs.get('pk'))
+        context_data['version_pk'] = product_item.pk
+        context_data['title'] = f"Версии товара {product_item.product_name.title()}"
+        return context_data
+
+
+# def toggle_activity(request, product_pk, version_pk):
+#     product = get_object_or_404(Product, pk=product_pk)
+#     version_item = get_object_or_404(Version, pk=version_pk, product=product)
+#     if version_item.is_active:
+#         version_item.is_active = False
+#     else:
+#         version_item.is_active = True
+#     version_item.save()
+#     return redirect(reverse('catalog:view_version', args=[product_pk]))
+
+
+def toggle_activity(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    version_item = get_object_or_404(Version, product=product)
+
+    if version_item.is_active:
+        version_item.is_active = False
+    else:
+        version_item.is_active = True
+    version_item.save()
+    return redirect(reverse('catalog:view_version', args=[pk]))
+
 
 class ContactsCreateView(CreateView):
     model = Contacts
@@ -95,20 +93,11 @@ class ContactsCreateView(CreateView):
         return HttpResponseRedirect(self.request.path_info)
 
 
-
-# def product(request, pk):
-#     product_name = Product.objects.get(pk=pk)
-#     context = {
-#         'object': product_name
-#     }
-#     return render(request, 'catalog/product_detail.html', context)
-
 class ProductDetailViews(DetailView):
     model = Product
 
     def get_context_data(self, *args, **kwargs):
         context_data = super().get_context_data(*args, **kwargs)
-
         product_name = Product.objects.get(pk=self.kwargs.get('pk'))
         context_data['object'] = product_name
         return context_data
