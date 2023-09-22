aa8c9f4c30430f1e33dc0a4c281e0ee40ceba6cb
evgeniya1011 <zhdavydova@mail.ru>
2023-09-22 05:21:52 +0500

    fix2 urls.py(hw22.1)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import ProductDetailViews, ProductCreateView, ContactsCreateView, ProductListView, VersionListView, \
-    VersionCreateView, toggle_activity
+from catalog.views import ProductDetailViews, ProductCreateView, ContactsCreateView, ProductListView, ProductUpdateWithVersionView
 
 app_name = CatalogConfig.name
 urlpatterns = [
@@ -10,8 +9,5 @@ urlpatterns = [
     path('contacts/', ContactsCreateView.as_view(), name='contacts'),
     path('view/<int:pk>/', ProductDetailViews.as_view(), name='view'),
     path('create/', ProductCreateView.as_view(), name='create'),
-    path('view_version/<int:pk>/', VersionListView.as_view(), name='view_version'),
-    path('create_version/', VersionCreateView.as_view(), name='create_version'),
-    path('activity/<int:pk>/', toggle_activity, name='toggle_activity'),
-    # path('activity/<int:product_pk>/<int:version_pk>/', toggle_activity, name='toggle_activity'),
+    path('edit/<int:pk>', ProductUpdateWithVersionView.as_view(), name='edit'),
 ]
