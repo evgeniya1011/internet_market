fd131886fc5c96808bec215669488dc83b248dce
evgeniya1011 <zhdavydova@mail.ru>
2023-09-22 05:22:51 +0500

    add productupdate(hw22.1)

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,11 @@
+from django.db import transaction
+from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy, reverse
-from django.views.generic import DetailView, CreateView, ListView
+from django.views.generic import DetailView, CreateView, ListView, UpdateView
 
-from catalog.forms import ProductForm, VersionForm
+from catalog.forms import ProductForm, VersionBaseInlineFormSet, VersionForm
 from catalog.models import Product, Contacts, Category, Version
 
 
@@ -27,53 +29,35 @@ class ProductListView(ListView):
         return context_data
 
 
-class VersionCreateView(CreateView):
-    model = Version
-    form_class = VersionForm
+class ProductUpdateWithVersionView(UpdateView):
+    model = Product
+    form_class = ProductForm
 
     def get_success_url(self, *args, **kwargs):
-        product_pk = self.object.product.pk
-        return reverse('catalog:view_version', kwargs={'pk': product_pk})
-
-
-class VersionListView(ListView):
-    model = Version
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset = queryset.filter(product=self.kwargs.get('pk'))
-        return queryset
+        return reverse('catalog:view', args=[self.object.id])
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
 
-        product_item = Product.objects.get(pk=self.kwargs.get('pk'))
-        context_data['version_pk'] = product_item.pk
-        context_data['title'] = f"Версии товара {product_item.product_name.title()}"
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1, formset=VersionBaseInlineFormSet)
+        if self.request.method == 'POST':
+            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = VersionFormset(instance=self.object)
         return context_data
 
-
-# def toggle_activity(request, product_pk, version_pk):
-#     product = get_object_or_404(Product, pk=product_pk)
-#     version_item = get_object_or_404(Version, pk=version_pk, product=product)
-#     if version_item.is_active:
-#         version_item.is_active = False
-#     else:
-#         version_item.is_active = True
-#     version_item.save()
-#     return redirect(reverse('catalog:view_version', args=[product_pk]))
-
-
-def toggle_activity(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    version_item = get_object_or_404(Version, product=product)
-
-    if version_item.is_active:
-        version_item.is_active = False
-    else:
-        version_item.is_active = True
-    version_item.save()
-    return redirect(reverse('catalog:view_version', args=[pk]))
+    def form_valid(self, form):
+        formset = self.get_context_data()['formset']
+        # self.object = form.save()
+        with transaction.atomic():
+            if form.is_valid():
+                self.object = form.save()
+                if formset.is_valid():
+                    formset.instance = self.object
+                    formset.save()
+                else:
+                    return super().form_invalid(form)
+        return super().form_valid(form)
 
 
 class ContactsCreateView(CreateView):
