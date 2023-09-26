89bea91d6fd63067d8e1e5c6cfb6734f60e97925
evgeniya1011 <zhdavydova@mail.ru>
2023-09-27 02:29:17 +0500

    add 'user'(hw22.2)

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from config import settings
+
 NULLABLE = {"blank": True, "null": True}
 
 
@@ -11,6 +13,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Цена за покупку')
     date_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     date_update = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Пользователь')
 
     def __str__(self):
         return f'{self.product_name}: {self.category}'
