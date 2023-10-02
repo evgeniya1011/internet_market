1c2c20a42ce7d4845f08dbdc98e4f08f1208dcd2
evgeniya1011 <zhdavydova@mail.ru>
2023-10-02 11:57:11 +0500

    fix hw(23.2)

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
 
 from blog.apps import BlogConfig
 from blog.views import ArticleCreateView, ArticleListView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
 
 app_name = BlogConfig.name
 urlpatterns = [
-    path('create/', ArticleCreateView.as_view(), name='create'),
+    path('create/', never_cache(ArticleCreateView.as_view()), name='create'),
     path('', ArticleListView.as_view(), name='list'),
     path('view/<int:pk>/', ArticleDetailView.as_view(), name='view'),
     path('edit/<int:pk>/', ArticleUpdateView.as_view(), name='edit'),
