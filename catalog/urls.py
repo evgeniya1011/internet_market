9585b2b0f997112d2d619331465a7559eeafa001
evgeniya1011 <zhdavydova@mail.ru>
2023-10-02 12:01:43 +0500

    fix hw(23.2)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page, never_cache
 
 from catalog.apps import CatalogConfig
-from catalog.views import ProductDetailViews, ProductCreateView, ContactsCreateView, ProductListView, ProductUpdateWithVersionView
+from catalog.views import ProductDetailViews, ProductCreateView, ContactsCreateView, ProductUpdateWithVersionView, ProductCategoryListView, CategoryListView
 
 app_name = CatalogConfig.name
 urlpatterns = [
-    path('', ProductListView.as_view(), name='list'),
+    path('', CategoryListView.as_view(), name='list'),
     path('contacts/', ContactsCreateView.as_view(), name='contacts'),
-    path('view/<int:pk>/', ProductDetailViews.as_view(), name='view'),
-    path('create/', ProductCreateView.as_view(), name='create'),
+    path('view/<int:pk>/', cache_page(60)(ProductDetailViews.as_view()), name='view'),
+    path('create/', never_cache(ProductCreateView.as_view()), name='create'),
     path('edit/<int:pk>', ProductUpdateWithVersionView.as_view(), name='edit'),
+    path('catalog/<int:pk>', ProductCategoryListView.as_view(), name='view_product')
 ]
